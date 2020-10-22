ff09e690996507d8cd230decb0fe6da972980a69
Carly Malatskey <cmalatskey@gmail.com>
2020-10-22 13:48:16 -0700

    changed definitions

---
+++ gitHubCommands.js
@@ -0,0 +1,29 @@
+// git add
+const gitAdd = "";
+
+// git commit
+const gitCommit = "";
+
+// git status
+const gitStatus = "";
+
+// git push
+const gitPush = "";
+
+// git merge
+const gitMerge = "";
+
+// git checkout
+const gitCheckout = "";
+
+// git branch
+const gitBranch = "";
+
+// git clone
+const gitClone = "";
+
+// git pull
+const gitPull = "";
+
+// git remote
+const gitRemote = "";
